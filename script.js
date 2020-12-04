ec7a3441bb3f3afa5e3bb0e1579e9ce3e9872c3f
unknown <sidicleiluiz@hotmail.com>
2020-12-03 23:31:21 -0300

    Pulando somente do chao

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-const dino = document.querySelector(".dino");
+const dino = document.querySelector('.dino');
+let isJumping = false;
 
 function handleKeyup(event) {
     if (event.keyCode === 32) {
-        console.log("pressionou espaço!");
+        if (!isJumping) {
+            jump();
+        }
     }
+}
+
+function jump () {
+    let position = 0;
+
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 200) {
+            clearInterval(upInterval);
+
+            //Descendo
+            let downIntervale = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downIntervale);
+                    isJumping = false;
+                }else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }else {
+            //Subindo
+        position += 20;
+
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener("keyup", handleKeyup);
